655112072703b6cf9c8416d4d155eccddbe6cdeb
Reid Beels <mail@reidbeels.com>
2009-10-03 23:12:23 -0700

    Added District#super_mayor to determine the super mayor for a given district.

--- models/district.rb
+++ models/district.rb
@@ -1,3 +1,8 @@
 class District < ActiveRecord::Base
   has_many :magic
+  has_many :users, :through => :magic
+  
+  def super_mayor
+    self.magic.find(:first, :order => 'mayorships DESC')
+  end
 end
